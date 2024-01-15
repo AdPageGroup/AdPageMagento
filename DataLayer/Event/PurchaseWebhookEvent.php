365a0ce7927751e53f167c26ac06314265055cd4
Jeroen Frenken <jeroen@frostup.com>
2024-01-15 14:54:40 +0100

    Fixing ecommerce formatting

--- DataLayer/Event/PurchaseWebhookEvent.php
+++ DataLayer/Event/PurchaseWebhookEvent.php
@@ -7,33 +7,31 @@ namespace AdPage\GTM\DataLayer\Event;
 use Magento\Framework\HTTP\ClientFactory;
 use Magento\Framework\Serialize\Serializer\Json;
 use Magento\Sales\Model\Order;
+use AdPage\GTM\DataLayer\Tag\Order\OrderItems;
+use Magento\Sales\Api\Data\OrderInterface;
+use AdPage\GTM\Util\PriceFormatter;
 use AdPage\GTM\Config\Config;
 
 class PurchaseWebhookEvent
 {
-    /**
-     * @var Json $json
-     */
     private $json;
-
-    /**
-     * @var ClientFactory $clientFactory
-     */
     private $clientFactory;
-
-    /**
-     * @var Config $config
-     */
     private $config;
+    private $orderItems;
+    private $priceFormatter;
 
     public function __construct(
         Json            $json,
         ClientFactory   $clientFactory,
+        OrderItems      $orderItems,
         Config          $config,
+        PriceFormatter  $priceFormatter
     ) {
         $this->json = $json;
         $this->clientFactory = $clientFactory;
+        $this->orderItems = $orderItems;
         $this->config = $config;
+        $this->priceFormatter = $priceFormatter;
     }
 
     private function call($event, $data)
@@ -54,11 +52,20 @@ class PurchaseWebhookEvent
         return $client->getStatus() == 200;
     }
 
-    public function purchase(Order $order, array $additionalInfo = [])
+    public function purchase(OrderInterface $order, array $additionalInfo = [])
     {
         $data = [
-            'user_data' => $order,
-            'ecommerce' => $order,
+            'user_data' => [],
+            'ecommerce' => [
+                'transaction_id' => $order->getIncrementId(),
+                'affiliation' => $this->config->getStoreName(),
+                'currency' => $order->getOrderCurrencyCode(),
+                'value' => $this->priceFormatter->format((float)$order->getGrandTotal()),
+                'tax' => $this->priceFormatter->format((float)$order->getTaxAmount()),
+                'shipping' => $this->priceFormatter->format((float)$order->getShippingInclTax()),
+                'coupon' => $order->getCouponCode(),
+                'items' => $this->orderItems->setOrder($order)->get()
+            ]
         ] + $additionalInfo;
         $this->call('purchase', $data);
     }
--- Observer/TriggerPurchaseWebhook.php
+++ Observer/TriggerPurchaseWebhook.php
@@ -5,6 +5,7 @@ namespace AdPage\GTM\Observer;
 use AdPage\GTM\DataLayer\Event\PurchaseWebhookEvent;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
+use Magento\Sales\Api\Data\OrderInterface;
 
 class OrderSaveCommitAfter implements ObserverInterface
 {
@@ -18,8 +19,8 @@ class OrderSaveCommitAfter implements ObserverInterface
 
     public function execute(Observer $observer)
     {
-        /** @var \Magento\Sales\Model\Order $order */
-        $order = $observer->getOrder();
+        /** @var OrderInterface $order */
+        $order = $observer->getData('order');
 
         if (!$order->dataHasChangedFor('total_paid') || $order->getGrandTotal() > $order->getTotalPaid()) {
             return;
