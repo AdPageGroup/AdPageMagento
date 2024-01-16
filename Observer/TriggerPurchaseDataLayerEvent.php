a7aac1f6cc512275c080bb02b112a88a5d02d75e
Jeroen Frenken <jeroen@frostup.com>
2024-01-16 13:02:03 +0100

    Changed method

--- Observer/TriggerPurchaseDataLayerEvent.php
+++ Observer/TriggerPurchaseDataLayerEvent.php
@@ -7,7 +7,6 @@ use Magento\Framework\Event\ObserverInterface;
 use Magento\Sales\Api\Data\OrderInterface;
 use AdPage\GTM\Api\CheckoutSessionDataProviderInterface;
 use AdPage\GTM\DataLayer\Event\Purchase as PurchaseEvent;
-use AdPage\GTM\DataLayer\Event\PurchaseWebhookEvent;
 use Psr\Log\LoggerInterface;
 use Exception;
 
@@ -16,18 +15,15 @@ class TriggerPurchaseDataLayerEvent implements ObserverInterface
     private CheckoutSessionDataProviderInterface $checkoutSessionDataProvider;
     private PurchaseEvent $purchaseEvent;
     private LoggerInterface $logger;
-    private PurchaseWebhookEvent $webhookEvent;
 
     public function __construct(
         CheckoutSessionDataProviderInterface $checkoutSessionDataProvider,
         PurchaseEvent $purchaseEvent,
-        PurchaseWebhookEvent $webhookEvent,
         LoggerInterface $logger
     ) {
         $this->checkoutSessionDataProvider = $checkoutSessionDataProvider;
         $this->purchaseEvent = $purchaseEvent;
         $this->logger = $logger;
-        $this->webhookEvent = $webhookEvent;
     }
 
     public function execute(Observer $observer)
@@ -40,19 +36,5 @@ class TriggerPurchaseDataLayerEvent implements ObserverInterface
             'purchase_event',
             $this->purchaseEvent->setOrder($order)->get()
         );
-
-        $this->logger->critical('TriggerPurchaseWebhook::execute(): has changed ' . $order->dataHasChangedFor('total_paid'));
-        $this->logger->critical('TriggerPurchaseWebhook::execute(): has grand total ' . $order->getGrandTotal());
-        $this->logger->critical('TriggerPurchaseWebhook::execute(): total paid ' . $order->getTotalPaid());
-
-        if (!$order->dataHasChangedFor('total_paid') || $order->getGrandTotal() > $order->getTotalPaid()) {
-            return;
-        }
-
-        try {
-            $this->webhookEvent->purchase($order, []);
-        } catch (\Exception $e) {
-            $this->logger->error($e->getMessage());
-        }
     }
 }
