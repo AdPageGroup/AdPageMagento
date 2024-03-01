949803ec98c301e2752f2c2a28b0dd8e459ab316
Jeroen Frenken <jeroen@frostup.com>
2024-03-01 09:17:25 +0100

    Updating marketing data more often

--- view/frontend/web/js/push.js
+++ view/frontend/web/js/push.js
@@ -1,55 +1,72 @@
-define(['googleTagManagerLogger'], function (logger) {
-    return function (eventData, message) {
-        window.AdPage_GTM_PAST_EVENTS = window.AdPage_GTM_PAST_EVENTS || [];
+define(["googleTagManagerLogger"], function (logger) {
+  return function (eventData, message) {
+    window.AdPage_GTM_PAST_EVENTS = window.AdPage_GTM_PAST_EVENTS || [];
 
-        const metaData = Object.assign({}, eventData.meta);
+    const metaData = Object.assign({}, eventData.meta);
 
-        const cleanEventData = Object.assign({}, eventData);
-        if (cleanEventData.meta) {
-            delete cleanEventData.meta;
-        }
+    const cleanEventData = Object.assign({}, eventData);
+    if (cleanEventData.meta) {
+      delete cleanEventData.meta;
+    }
 
-        if (cleanEventData.length === 0) {
-            return;
-        }
+    if (cleanEventData.length === 0) {
+      return;
+    }
 
-        if (metaData && metaData.allowed_pages && metaData.allowed_pages.length > 0
-            && false === metaData.allowed_pages.some(page => window.location.pathname.includes(page))) {
-            logger('Warning: Skipping event, not in allowed pages', window.location.pathname, eventData);
-            return;
-        }
+    if (
+      metaData &&
+      metaData.allowed_pages &&
+      metaData.allowed_pages.length > 0 &&
+      false ===
+        metaData.allowed_pages.some((page) =>
+          window.location.pathname.includes(page)
+        )
+    ) {
+      logger(
+        "Warning: Skipping event, not in allowed pages",
+        window.location.pathname,
+        eventData
+      );
+      return;
+    }
 
-        // Prevent the same event from being triggered twice, when containing the same data
-        const eventHash = btoa(encodeURIComponent(JSON.stringify(cleanEventData)));
-        if (window.AdPage_GTM_PAST_EVENTS.includes(eventHash)) {
-            logger('Warning: Event already triggered', eventData);
-            return;
-        }
+    // Prevent the same event from being triggered twice, when containing the same data
+    const eventHash = btoa(encodeURIComponent(JSON.stringify(cleanEventData)));
+    if (window.AdPage_GTM_PAST_EVENTS.includes(eventHash)) {
+      logger("Warning: Event already triggered", eventData);
+      return;
+    }
 
-        if (!message) {
-            message = 'push (unknown) [unknown]';
-        }
+    if (!message) {
+      message = "push (unknown) [unknown]";
+    }
 
-        logger(message, eventData);
-        window.dataLayer = window.dataLayer || [];
-        window.dataLayer.push({ecommerce: null});
+    logger(message, eventData);
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({ ecommerce: null });
 
-        if (window.taggingHelpers) {
-            cleanEventData.marketing = window.taggingHelpers.getMarketingObject();
-            cleanEventData.device = window.taggingHelpers.getDeviceInfo();
-        }
+    if (window.taggingHelpers) {
+      cleanEventData.marketing = window.taggingHelpers.getMarketingObject();
+      cleanEventData.device = window.taggingHelpers.getDeviceInfo();
+    }
 
-        if (cleanEventData.event === 'trytagging_begin_checkout') {
-            // Post marketingObject to server
-            const xhr = new XMLHttpRequest();
-            xhr.open('POST', '/rest/V1/trytagging/save', true);
-            xhr.setRequestHeader('Content-Type', 'application/json');
-            xhr.send(JSON.stringify({
-                jsonData: cleanEventData.marketing || {},
-            }));
-        }
+    if (
+      cleanEventData.event === "trytagging_begin_checkout" ||
+      cleanEventData.event === "trytagging_view_cart" ||
+      cleanEventData.event === "trytagging_add_to_cart"
+    ) {
+      // Post marketingObject to server
+      const xhr = new XMLHttpRequest();
+      xhr.open("POST", "/rest/V1/trytagging/save", true);
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.send(
+        JSON.stringify({
+          jsonData: cleanEventData.marketing || {},
+        })
+      );
+    }
 
-        window.dataLayer.push(cleanEventData);
-        window.AdPage_GTM_PAST_EVENTS.push(eventHash);
-    };
+    window.dataLayer.push(cleanEventData);
+    window.AdPage_GTM_PAST_EVENTS.push(eventHash);
+  };
 });
